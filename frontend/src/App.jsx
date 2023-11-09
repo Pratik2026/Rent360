9660b114d8e39843868760f083b054e0c3337fa8
Prateek Jha <prateekjha1204@gmail.com>
2023-11-09 23:07:05 +0530

    Made rent page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Rent from "./pages/rent";
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="rent" element={<Rent />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
